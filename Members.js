6dbde1d1ebad129dcfa93e9b6c56d512689ca9cc
AbdAllahEl-Gamal <abdallah.algamal93@gmail.com>
2019-09-06 14:07:32 +0200

    My first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Abdallah',
+        email: 'sss@hh.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Mohamed',
+        email: 'mmm@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Ahmed',
+        email: 'aaa@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const members = require('./Members');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+        title: 'Members App',
+        members
+    }
+));
+
+// Set Static Folder
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
